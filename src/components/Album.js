3101cc4e91cad98d5ad151177fb90bff8e5d15db
Luke Strama <strama@ualberta.ca>
2018-09-12 15:59:19 -0600

    jams-library-checkpoint

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Album = () => (
-    <section className="album">
-        This will be album information
-    </section>
-);
+class Album extends Component {
+    render() {
+        return (
+            <section className="album">
+                {this.props.match.params.slug} This will be album information
+            </section>
+        );        
+    }
+} 
 
 export default Album;
